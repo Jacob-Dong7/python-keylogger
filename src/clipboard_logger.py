33f66d4072dffe201cb3fe49aee6cde431d8ba0d
JDOT <JDON219206@gmail.com>
2026-07-04 21:50:58 +1000

    feat: began clip logger

---
+++ src/clipboard_logger.py
@@ -0,0 +1,11 @@
+import pyperclip
+import time
+
+class Clip():
+    def __init__(self):
+        return
+    
+    def start(self):
+        value = ""
+        curr_value
+        return
--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,12 @@ class Logger():
 
     def __init__(self):
         return
-    
+
+    def start(self):
+        logger = keyboard.Listener(on_press=self.press)
+        logger.start()
+        logger.join()
+
     def press(self, key):
         if key == keyboard.Key.esc:
             print("Exiting...")
@@ -12,9 +17,4 @@ class Logger():
             print(f"Key: {key.char}")
         except AttributeError:
             print(f"Special Key: {key}")
-
-    def start(self):
-        logger = keyboard.Listener(on_press=self.press)
-        logger.start()
-        logger.join()
 
