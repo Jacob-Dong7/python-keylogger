2020508a899fe0fa4d4ebc4982198351efeba727
JDOT <JDON219206@gmail.com>
2026-07-04 17:50:00 +1000

    feat: implemented the basic logger for key inputs.

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.logger import Logger
+def main():
+    logger = Logger()
+    logger.start()
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ src/logger.py
@@ -0,0 +1,20 @@
+from pynput import keyboard
+class Logger():
+
+    def __init__(self):
+        return
+    
+    def press(self, key):
+        if key == keyboard.Key.esc:
+            print("Exiting...")
+            return False
+        try:
+            print(f"Key: {key.char}")
+        except AttributeError:
+            print(f"Special Key: {key}")
+
+    def start(self):
+        logger = keyboard.Listener(on_press=self.press)
+        logger.start()
+        logger.join()
+
