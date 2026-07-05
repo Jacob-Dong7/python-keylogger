580b77a032463777c538cfe4a5a4ec23e201dd6a
JDOT <JDON219206@gmail.com>
2026-07-05 15:51:23 +1000

    feat: implemented clipboard logger

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from src.logger import Logger
+from src.clipboard_logger import Clip
 def main():
-    logger = Logger()
-    logger.start()
-    return
+    while True:
+        print("[-1] Exit [1] Keylogger [2] Clipboard Logger")
+        option = input("Select: ")
+        if option == "1":
+            logger = Logger()
+            logger.start()
+        elif option == "2":
+            logger = Clip()
+            logger.start()
+        elif option == "-1":
+            print("Exiting...")
+            return
 
 if __name__ == "__main__":
     main()
--- src/clipboard_logger.py
+++ src/clipboard_logger.py
@@ -7,5 +7,11 @@ class Clip():
     
     def start(self):
         value = ""
-        curr_value
-        return
+        while True:
+            time.sleep(5)
+            curr_value = pyperclip.paste()
+            if curr_value != value:
+                value = curr_value
+                print(curr_value)
+            if curr_value == "EXIT":
+                return
